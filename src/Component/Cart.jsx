3a9a2911a96ca9df885fb54b48af4b38929a1d99
Vignesha Shanmugam <svignesha18@gmail.com>
2024-05-24 12:37:31 +0530

    new project

---
+++ src/Component/Cart.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { mycontext } from "../App";
+import Card from "./Card";
+
+const Cart = () => {
+  
+  return (
+      <div>
+        <Card />
+      </div>
+  );
+};
+
+export default Cart;
